2326de74d400ac8c2f473b59146d5fa52c58762a
Vike <62164219+VikeMK@users.noreply.github.com>
2022-06-11 00:05:37 +1000

    Add dockerfile and launch.json for API, added basic SQLite code, and made a VSCode workspace file for the repo (#2)

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,36 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
 from starlette.routing import Route
+import aiosqlite
+
+# Hardcoded location for now, eventually should be managed as a docker or kubernetes volume 
+DB_PATH = "mkc.db"
+
+async def init_db():
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("CREATE TABLE IF NOT EXISTS users (name TEXT PRIMARY KEY)")
+        await db.commit()
+
+async def add_user(request: Request) -> Response:
+    async with aiosqlite.connect(DB_PATH) as db:
+        await db.execute("INSERT INTO users VALUES (?)", [request.query_params["name"]])
+        await db.commit()
+    return Response(status_code=200)
+
+async def list_users(request: Request) -> JSONResponse:
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute("SELECT name FROM users") as cursor:
+            names = [row[0] for row in await cursor.fetchall()]
+    return JSONResponse({'users': names})
 
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
-app = Starlette(debug=True, routes=[
+routes = [
     Route('/', homepage),
-])
+    Route('/user/list', list_users),
+    Route('/user', add_user, methods=["POST"])
+]
+
+app = Starlette(debug=True, routes=routes, on_startup=[init_db])
