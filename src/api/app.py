4c93aab193e38278d1954e7255b720aeb286e025
XOlifreX <olivier.luyckx@live.be>
2022-07-12 23:58:43 +0200

    Added MinIO docker container + API endpoints for read/writing of a file

--- src/api/app.py
+++ src/api/app.py
@@ -1,8 +1,18 @@
+import io
+
+import aiosqlite
+from minio import Minio
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 from starlette.routing import Route
-import aiosqlite
+
+minio_client = Minio(
+    "s3-emulator:9000", 
+    access_key="mkcdev", 
+    secret_key="mk4thewin",
+    secure=False
+)
 
 DB_PATH = "/var/lib/mkc-api/data/mkc.db"
 
@@ -32,9 +42,59 @@ async def list_users(request: Request) -> JSONResponse:
 async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
+async def minio_read(request: Request):
+    try:
+        data = await request.json()
+        bucket_name = data['bucket']
+        file_name = data['file']
+    except RuntimeError:
+        return JSONResponse({'error':'No correct body send'})
+
+    print(bucket_name)
+    print(file_name)
+
+    try:
+        response = minio_client.get_object(bucket_name, file_name)
+        # Read data from response.
+        return JSONResponse({
+            '{b} - {fn}'.format(b=bucket_name, fn=file_name): 
+            '{res}'.format(res=response.data.decode())
+        })
+    finally:
+        response.close()
+        response.release_conn()
+
+async def minio_write(request):
+    try:
+        data = await request.json()
+        bucket_name = data['bucket']
+        file_name = data['file']
+        message = data['message']
+    except RuntimeError:
+        return JSONResponse({'error':'No correct body send'})
+
+    if not minio_client.bucket_exists(bucket_name):
+        print('creating {bucket}...'.format(bucket=bucket_name))
+        minio_client.make_bucket(bucket_name)
+
+    message = message.encode('utf-8')
+
+    result = minio_client.put_object(
+        bucket_name, 
+        file_name, 
+        io.BytesIO(message), 
+        length=len(message)
+    )    
+
+    print(result.object_name)
+
+    return JSONResponse(result.object_name)
+
 routes = [
     Route('/api', homepage),
     Route('/api/user/list', list_users),
+    Route('/api/minio', minio_read),
+    Route('/api/minio', minio_write, methods=["POST"]),
     Route('/api/user', add_user, methods=["POST"])
 ]
 
