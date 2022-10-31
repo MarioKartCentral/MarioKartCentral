5947c2f3918da5d0c7f44998139e8574798cea85
VikeMK <62164219+VikeMK@users.noreply.github.com>
2022-10-31 00:34:47 +0000

    Forgot to hit save on these three files, oops

--- src/frontend/src/i18n/es/index.ts
+++ src/frontend/src/i18n/es/index.ts
@@ -6,7 +6,14 @@ const es: Translation = {
   // TODO
   SUMMARY:
     "We are the home of <strong>Mario Kart Tournaments</strong>, where you'll find <strong>tournaments</strong> for all players—whether you're looking for a casual or competitive competition, we've got you covered! We hold events for <strong>Mario Kart 8 Deluxe, Tour, and more</strong>, with a variety of matches happening weekly!",
-  LANGUAGE: 'español'
+  LANGUAGE: 'español',
+  NAVBAR: {
+    TOURNAMENTS: 'Tournaments',
+    TIME_TRIALS: 'Time Trials',
+    LOUNGE: 'Lounge',
+    REGISTRY: 'Registry',
+    DISCORD: 'Discord',
+  }
 };
 
 export default es;
