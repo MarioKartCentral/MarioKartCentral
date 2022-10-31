99790331286a466408ba55e3c52bccfc901af7fc
VikeMK <62164219+VikeMK@users.noreply.github.com>
2022-10-31 09:47:45 +0000

    Make redis connection load via environment variable

--- src/api/app.py
+++ src/api/app.py
@@ -10,18 +10,19 @@ from starlette.responses import JSONResponse, Response, RedirectResponse
 from starlette.routing import Route
 import redis.asyncio as redis
 
-hasher = PasswordHasher()
-redis_conn = redis.Redis(host='redis', port=6379, decode_responses=True)
-
 AWS_ACCESS_KEY_ID=os.environ["S3_ACCESS_KEY"]
 AWS_SECRET_ACCESS_KEY=os.environ["S3_SECRET_KEY"]
 AWS_ENDPOINT_URL=os.environ["S3_ENDPOINT"]
 ADMIN_EMAIL=os.environ["API_ADMIN_EMAIL"]
 ADMIN_PASSWORD=os.environ["API_ADMIN_PASSWORD"]
+REDIS_URL=os.environ["REDIS_URL"]
 
 DB_PATH = "/var/lib/mkc-api/data/mkc.db"
 DEBUG = False
 RESET_DATABASE = False
+
+hasher = PasswordHasher()
+redis_conn = redis.from_url(REDIS_URL, decode_responses=True)
 
 if DEBUG:
     import debugpy
