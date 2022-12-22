dbbf2e1c301fd590fca8b5ea83f91204e17b0021
cyndaquilx <ultramk8@gmail.com>
2022-12-22 04:08:24 +0000

    improve basic functionality

--- src/api/api/endpoints/tournaments.py
+++ src/api/api/endpoints/tournaments.py
@@ -9,15 +9,19 @@ import json
 
 @require_permission(permissions.WRITE_S3)
 async def create_tournament(request: Request) -> JSONResponse:
+    body = await request.json()
     try:
-        tournament_name = request.query_params['name']
-        date = int(request.query_params['date'])
-        game = request.query_params['game']
-        tournament_status = int(request.query_params['status'])
-        description = request.query_params['description']
+        # there should be way more parameters than this in the final version;
+        # however these are the minimum to make sure the basic functionality works
+        tournament_name = body['name']
+        date = int(body['date'])
+        game = body['game']
+        tournament_status = int(body['status'])
+        description = body['description']
     except RuntimeError:
         return JSONResponse({'error': 'No correct body send'})
 
+    # store minimal data about each tournament in the SQLite DB
     async with connect_db() as db:
         cursor = await db.execute(
             "INSERT INTO tournaments(name, date, game, status) VALUES (?, ?, ?, ?)",
@@ -25,46 +29,32 @@ async def create_tournament(request: Request) -> JSONResponse:
         tournament_id = cursor.lastrowid
         await db.commit()
 
-    s3_json = {"id": tournament_id, "description": description}
+    # store more detailed information about the tournament in object storage
+    s3_json = {"id": tournament_id,
+                "name": tournament_name,
+                "date": date,
+                "game": game,
+                "status": tournament_status,
+                "description": description}
     s3_message = bytes(json.dumps(s3_json).encode('utf-8'))
     session = aiobotocore.session.get_session()
     async with create_s3_client(session) as s3_client:
         result = await s3_client.put_object(Bucket='tournaments', Key=f'{tournament_id}.json', Body=s3_message)
-    print(result)
-    return JSONResponse({"id": tournament_id,
-                    "name": tournament_name,
-                    "date": date,
-                    "game": game,
-                    "status": tournament_status,
-                    "description": description})
+    return JSONResponse(s3_json)
 
 @require_permission(permissions.READ_S3)
 async def tournament_info(request: Request) -> JSONResponse:
     tournament_id = request.path_params['id']
-    async with connect_db() as db:
-        async with db.execute("SELECT name, date, game, status FROM tournaments WHERE id = ?", (tournament_id, )) as cursor:
-            row = await cursor.fetchone()
-            if row is None:
-                return JSONResponse({'error':'Invalid tournament ID'}, status_code=401)
-            tournament_name = row[0]
-            date = row[1]
-            game = row[2]
-            status = row[3]
     session = aiobotocore.session.get_session()
     async with create_s3_client(session) as s3_client:
-        response = await s3_client.get_object(Bucket='tournaments', Key=f'{tournament_id}.json')
+        try:
+            response = await s3_client.get_object(Bucket='tournaments', Key=f'{tournament_id}.json')
+        except s3_client.exceptions.NoSuchKey as e:
+            return JSONResponse({'error':'Invalid tournament ID'}, status_code=401)
         async with response['Body'] as stream:
             body = await stream.read()
             json_body = json.loads(body)
-            description = json_body['description']
-    return JSONResponse({
-        "id": tournament_id,
-        "name": tournament_name,
-        "date": date,
-        "game": game,
-        "status": status,
-        "description": description
-    })
+    return JSONResponse(json_body)
         
 routes = [
     Route('/api/tournaments/create', create_tournament, methods=["POST"]),
