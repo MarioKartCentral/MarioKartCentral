ce167ac45d5af5b868b82d88b7de80950e3387a5
VikeMK <62164219+VikeMK@users.noreply.github.com>
2023-04-09 13:53:17 +0000

    S3 wrapper changes

--- src/backend/api/app.py
+++ src/backend/api/app.py
@@ -1,7 +1,7 @@
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from api import settings
-from api.data import init_db, init_s3
+from api.data import init_db, init_s3, close_s3
 from api.endpoints import authservice, redisservice, roleservice, s3service, userservice, tournaments, tournament_registration, player_registry
 from api.utils.middleware import ProblemHandlingMiddleware
 
@@ -25,4 +25,4 @@ middleware = [
     Middleware(ProblemHandlingMiddleware)
 ]
 
-app = Starlette(debug=True, routes=routes, on_startup=[init_db, init_s3], middleware=middleware)
+app = Starlette(debug=True, routes=routes, on_startup=[init_db, init_s3], on_shutdown=[close_s3], middleware=middleware)
