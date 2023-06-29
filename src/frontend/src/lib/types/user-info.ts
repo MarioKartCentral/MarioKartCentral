db1e7fbaf0cb6e901133401608fd8e7a474844c6
cyndaquilx <ultramk8@gmail.com>
2023-06-29 20:22:45 +0000

    create basic user info store

---
+++ src/frontend/src/lib/types/user-info.ts
@@ -0,0 +1,8 @@
+export type UserInfo = {
+    user_id: number | null;
+    player_id: number | null;
+    name: string | null;
+    country_code: string | null;
+    discord_id: string | null;
+    is_checked: boolean;
+}
