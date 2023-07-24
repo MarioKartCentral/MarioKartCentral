66b9f610a188b3dee2156124600a998936ac91d1
cyndaquilx <ultramk8@gmail.com>
2023-07-24 17:30:26 +0000

    remove discord tag from user settings on frontend

--- src/frontend/src/lib/types/user-settings.ts
+++ src/frontend/src/lib/types/user-settings.ts
@@ -1,7 +1,6 @@
 export type UserSettings = {
     user_id: number;
     avatar: string | null;
-    discord_tag: string | null;
     about_me: string | null;
     language: string;
     color_scheme: string;
