8aa9427d631760059e3b4d9390498e365c21a60b
cyndaquilx <ultramk8@gmail.com>
2023-07-26 22:05:50 +0000

    create Table component

---
+++ src/frontend/src/lib/components/common/Table.svelte
@@ -0,0 +1,23 @@
+<table>
+   <slot/> 
+</table>
+
+<style>
+    table {
+        background-color: rgba(127, 127, 127, 0.6);
+        border-collapse: collapse;
+        width: 100%;
+    }
+    table :global(thead) {
+        background-color: rgb(0, 128, 0, 0.6);
+    }
+    table :global(td) {
+        padding: 10px;
+        text-align: center;
+    }
+    /* Alternating row background colors: in an each block,
+        put the class name as row-{i%2} */
+    table :global(.row-1) {
+        background-color:rgba(192, 192, 192, 0.6);
+    }
+</style>
