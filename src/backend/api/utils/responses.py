4f07b13309db23c43e2598dedd205f44fa0c3daf
cyndaquilx <ultramk8@gmail.com>
2023-07-26 21:03:39 +0000

    change strict mode in bind_request_query

--- src/backend/api/utils/responses.py
+++ src/backend/api/utils/responses.py
@@ -23,7 +23,7 @@ def bind_request_query(type: Type[TBind]):
             query_as_json = msgspec.json.encode(dict(request.query_params))
 
             try:
-                body = msgspec.json.decode(query_as_json, type=type)
+                body = msgspec.json.decode(query_as_json, type=type, strict=False)
             except msgspec.ValidationError as e:
                 raise Problem("Invalid query string parameter", detail=str(e), status=400)
 
