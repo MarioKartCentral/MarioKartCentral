9a0b6a7ce44f1be6036763dabd94d0a4dd0549c6
cyndaquilx <ultramk8@gmail.com>
2023-08-23 20:26:06 +0000

    create LinkButton component

---
+++ src/frontend/src/lib/components/common/LinkButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let href: string;
+</script>
+
+<a href={href}><slot/></a>
+
+<style>
+    a {
+        text-decoration: none;
+        color: white;
+        background-color: rgba(255, 255, 255, 0.3);
+        border-radius: 10px;
+        padding: 5px 10px;
+    }
+    a:hover {
+        background-color: rgba(255, 255, 255, 0.4);
+    }
+</style>
