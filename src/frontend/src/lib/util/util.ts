e87c632cf80e35b041737e3cbb4e68dad3f9ab1b
cyndaquilx <ultramk8@gmail.com>
2023-09-08 03:41:27 +0000

    fix addPermission

--- src/frontend/src/lib/util/util.ts
+++ src/frontend/src/lib/util/util.ts
@@ -1,6 +1,6 @@
 import { getContext } from 'svelte';
 
 export function addPermission(permission: string) {
-    let ctx: any = getContext('page-init');
+    const ctx: any = getContext('page-init');
     ctx.addPermission(permission);
 }
