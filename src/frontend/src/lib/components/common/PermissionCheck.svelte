8b3adecf6c25b58ba9e09f433f1aac519c7b4947
cyndaquilx <ultramk8@gmail.com>
2023-09-14 18:44:40 +0000

    create PermissionCheck component

---
+++ src/frontend/src/lib/components/common/PermissionCheck.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { user } from "$lib/stores/stores";
+    import type { UserInfo } from "$lib/types/user-info";
+
+    export let permission: string;
+
+    let user_info: UserInfo;
+    user.subscribe((value) => {
+        user_info = value;
+    });
+</script>
+
+{#if user_info.permissions.includes(permission)}
+    <slot/>
+{:else}
+    You do not have permission to view this page.
+{/if}
