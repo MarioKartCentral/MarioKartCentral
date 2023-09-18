89ac5e6c06f7f6f583eee7c9b6d33bfa65191d9c
devotyz <devotyboat@gmail.com>
2023-09-18 22:49:13 +0000

    trying filters

---
+++ src/frontend/src/lib/components/registry/players/PlayerFilter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let filters;
+  const list = [
+    { value: null, label: 'Show All' },
+    { value: 'mk8dx', label: 'Show with MK8DX FC' },
+    { value: 'mkw', label: 'Show with MKW FC' },
+    { value: 'mkt', label: 'Show with MKT FC' },
+    { value: 'mk7', label: 'Show with 3DS FC' },
+    { value: 'mk8', label: 'Show with NNID' },
+  ];
+
+</script>
+
+<div>
+  <select bind:value={filters.game} on:click={() => console.log(filters)}>
+    {#each list as { value, label }}
+      <option value={value}>{label}</option>
+    {/each}
+  </select>
+</div>
--- src/frontend/src/routes/[lang]/registry/players/+page.svelte
+++ src/frontend/src/routes/[lang]/registry/players/+page.svelte
@@ -3,23 +3,42 @@
   import type { PlayerInfo } from '$lib/types/player-info';
   import Section from '$lib/components/common/Section.svelte';
   import PlayerList from '$lib/components/registry/players/PlayerList.svelte';
+  import PlayerFilter from '$lib/components/registry/players/PlayerFilter.svelte';
 
   let players: PlayerInfo[] = [];
+  let filters = {
+    game: null,
+    name: null,
+    fc: null,
+  };
 
-  onMount(async () => {
-    const res = await fetch('/api/registry/players?detailed=true');
+  async function fetchData() {
+    players = [];
+    let url = '/api/registry/players?detailed=true';
+    if (filters.game != null) {
+      url += '&game=' + filters.game;
+    }
+    const res = await fetch(url);
     if (res.status === 200) {
       const body = await res.json();
       for (let t of body) {
         players.push(t);
       }
       players = players;
-      console.log('players : ', players);
     }
-  });
+  }
+
+  $: {
+    if (filters.game != null) {
+      fetchData();
+    }
+  }
+
+  onMount(fetchData);
 </script>
 
 <Section header="Player Listing">
+  <PlayerFilter bind:filters />
   {players.length} players
   <PlayerList {players} />
 </Section>
