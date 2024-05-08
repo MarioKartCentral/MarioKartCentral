c6a03e2fa627966d61664c3026f66f7e143da225
cyndaquilx <ultramk8@gmail.com>
2024-05-08 21:01:38 +0000

    refactor team edit pages, combine similar parts to one component

---
+++ src/frontend/src/lib/components/common/LanguageSelect.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import LL from "$i18n/i18n-svelte";
+
+    export let language = 'en-us';
+
+    const languages = [
+      { value: 'de', getLang: 'DE' },
+      { value: 'en-gb', getLang: 'EN_GB' },
+      { value: 'en-us', getLang: 'EN_US' },
+      { value: 'fr', getLang: 'FR' },
+      { value: 'es', getLang: 'ES' },
+      { value: 'ja', getLang: 'JA' },
+    ];
+</script>
+
+<select name="language" value={language}>
+    {#each languages as l}
+      <option value={l.value}>{$LL.LANGUAGES[l.getLang]()}</option>
+    {/each}
+</select>
