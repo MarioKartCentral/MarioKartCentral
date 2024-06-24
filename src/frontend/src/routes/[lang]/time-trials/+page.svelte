8faa13fce1fe966615ae7a955d674053a103c0e5
embrxr <andrew8162003@gmail.com>
2024-06-24 15:44:00 +0000

    improved records desc

--- src/frontend/src/routes/[lang]/time-trials/+page.svelte
+++ src/frontend/src/routes/[lang]/time-trials/+page.svelte
@@ -16,24 +16,26 @@
 
 <div>
   <p>
-    MKRecords is the hub for Mario Kart Time Trials. Here, you'll find the most 
+    This page is currently a work in progress, but it will eventually be the hub for everything 
+    time trials. For now, players can find leaderboards on several other sites.
+    This is the hub of Mario Kart Time Trials. Here, you'll find the most 
     up-to-date rankings for Mario Kart 8 Deluxe, Mario Kart 8, Mario Kart 7, and 
     Mario Kart Wii, with every category imagineable. Submit your times
     to see where you stack up!
   </p>
 </div>
 
-<div style="display: flex; flex-wrap: wrap; gap: 10px;">
-  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent;">
+<div style="display: flex; flex-wrap: wrap; gap: 10px; justify-content: center;">
+  <Button style="flex: 1 1 calc(33.333% - 10px); paddinwg: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
     <img src="$lib/assets/logo_mk8dx.png" alt="Mario Kart 8 Deluxe" style="max-width: 100%;">
   </Button>
-  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent;">
+  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
     <img src="$lib/assets/logo_mk8.png" alt="Mario Kart 8" style="max-width: 100%;">
   </Button>
-  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent;">
+  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
     <img src="$lib/assets/logo_mk7.png" alt="Mario Kart 7" style="max-width: 100%;">
   </Button>
-  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent;">
-    <img src="$lib/assets/logo_mkw.png" alt="Mario Kart Wii" style="max-width: 100%;">
+  <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
+    <img src="lib/assets/logo_mkw.png" alt="Mario Kart Wii" style="max-width: 100%;">
   </Button>
 </div>
