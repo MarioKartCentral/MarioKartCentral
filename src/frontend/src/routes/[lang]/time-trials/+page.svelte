0d65f00c37c906522196b85fd84c0a48dfe4ba97
embrxr <andrew8162003@gmail.com>
2024-06-24 17:14:54 +0000

    game paths

--- src/frontend/src/routes/[lang]/time-trials/+page.svelte
+++ src/frontend/src/routes/[lang]/time-trials/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import Section from '$lib/components/common/Section.svelte';
-  import { addPermission, permissions } from '$lib/util/util';
-  import { user } from '$lib/stores/stores';
-  import type { UserInfo } from '$lib/types/user-info';
   import Button from '$lib/components/common/buttons/Button.svelte';
-  import { page } from '$app/stores';
   import LL from '$i18n/i18n-svelte';
+  import MK8DX from '$lib/assets/logo_mk8dx.png';
+  import MK8 from '$lib/assets/logo_mk8.png';
+  import MK7 from '$lib/assets/logo_mk7.png';
+  import MKW from '$lib/assets/logo_mkw.png';
+
 </script>
 
 <Section header={$LL.NAVBAR.TIME_TRIALS()}>
@@ -27,15 +27,15 @@
 
 <div style="display: flex; flex-wrap: wrap; gap: 10px; justify-content: center;">
   <Button style="flex: 1 1 calc(33.333% - 10px); paddinwg: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
-    <img src="$lib/assets/logo_mk8dx.png" alt="Mario Kart 8 Deluxe" style="max-width: 100%;">
+    <img src={MK8DX} alt="Mario Kart 8 Deluxe" style="max-width: 100%;">
   </Button>
   <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
-    <img src="$lib/assets/logo_mk8.png" alt="Mario Kart 8" style="max-width: 100%;">
+    <img src={MK8} alt="Mario Kart 8" style="max-width: 100%;">
   </Button>
   <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
-    <img src="$lib/assets/logo_mk7.png" alt="Mario Kart 7" style="max-width: 100%;">
+    <img src={MK7} alt="Mario Kart 7" style="max-width: 100%;">
   </Button>
   <Button style="flex: 1 1 calc(33.333% - 10px); padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;">
-    <img src="lib/assets/logo_mkw.png" alt="Mario Kart Wii" style="max-width: 100%;">
+    <img src={MKW} alt="Mario Kart Wii" style="max-width: 100%;">
   </Button>
 </div>
