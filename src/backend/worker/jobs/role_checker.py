f251d5e2680cac6a334ad6f3544fcc3983341ace
cyndaquilx <ultramk8@gmail.com>
2024-07-30 18:42:35 +0000

    add job to remove expired roles

---
+++ src/backend/worker/jobs/role_checker.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+from worker.data import handle
+from common.data.commands import RemoveExpiredRolesCommand
+from worker.jobs import Job
+
+class RemoveExpiredRolesJob(Job):
+    @property
+    def name(self):
+        return "Remove Expired Roles"
+    
+    @property
+    def delay(self):
+        return timedelta(minutes=1)
+    
+    async def run(self):
+        await handle(RemoveExpiredRolesCommand())
+        print("yes")
+
+_jobs: list[Job] = []
+
+def get_jobs():
+    if not _jobs:
+        _jobs.append(RemoveExpiredRolesJob())
+    return _jobs
