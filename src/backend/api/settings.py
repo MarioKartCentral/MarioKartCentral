70ca3ac4cf5611da999b14e77e52f14cf3bcbbd9
billypom on debian <billypompey@icloud.com>
2024-09-14 15:22:06 -0400

    tournaments page styling

--- src/backend/api/settings.py
+++ src/backend/api/settings.py
@@ -4,8 +4,8 @@ from starlette.config import Config
 
 config = Config()
 
-DEBUG = config('DEBUG', cast=bool, default=False)
-RESET_DATABASE = config('RESET_DATABASE', cast=bool, default=False)
+DEBUG = config("DEBUG", cast=bool, default=False)
+RESET_DATABASE = config("RESET_DATABASE", cast=bool, default=False)
 AWS_ACCESS_KEY_ID = config("S3_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = config("S3_SECRET_KEY", cast=Secret)
 AWS_ENDPOINT_URL = config("S3_ENDPOINT")
