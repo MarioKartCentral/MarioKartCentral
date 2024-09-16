3f0f095ffb37fb0be5cd9bf4a16e55502b5e9744
billy <billypompey@icloud.com>
2024-09-15 20:45:58 -0400

    Tournament listing (#136)
    * tournaments page styling
    
    * smaller tourney name text + button = view tournament + series href + view tournament href
    
    * a tag centering fix
    
    * remove unnecessary css
    
    * add binding for series orphaned tournament, add registrations_open and use_series_logo to TournamentListItem type, adjusted button sizes and conditional view vs. register for tournament buttons

--- src/frontend/src/lib/components/tournaments/TournamentPageItem.svelte
+++ src/frontend/src/lib/components/tournaments/TournamentPageItem.svelte
@@ -4,6 +4,7 @@
   import TypeBadge from '../badges/TypeBadge.svelte';
   import ModeBadge from '../badges/ModeBadge.svelte';
   import GameBadge from '../badges/GameBadge.svelte';
+  import Button from '$lib/components/common/buttons/Button.svelte';
   export let tournament: TournamentListItem;
 
   let date_start = new Date(tournament.date_start * 1000);
@@ -11,39 +12,131 @@
 
   $: tournament_type = tournament.is_squad ? (tournament.teams_allowed ? 'Team' : 'Squad') : 'Solo';
   let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  // Appends leading 0 to single digit numbers
+  const zeroPad = (num: number) => String(num).padStart(2, '0');
 </script>
 
 <div class="container">
-  <!-- <div>{tournament.id}</div> -->
-  <div class="name">
-    <h3><a href="/{$page.params.lang}/tournaments/details?id={tournament.id}">{tournament.name}</a></h3>
+  <div class="grid-container">
+    <!-- Informational Section -->
+    <div class="information flex flex-col align-middle items-center justify-center text-center">
+      <div class="name">
+        <h3>
+          <a
+            class="text-lg font-bold hover:text-emerald-400 p-1"
+            href="/{$page.params.lang}/tournaments/details?id={tournament.id}"
+            >{tournament.name}
+          </a>
+        </h3>
+      </div>
+      <div class="dates flex flex-row">
+        <div class="start-date flex flex-col text-center p-1">
+          <b>
+            {months[date_start.getMonth()].toUpperCase()}
+          </b>
+          <b>
+            {zeroPad(date_start.getDate())}
+          </b>
+        </div>
+        <div class="dash flex flex-row text-center align-middle p-1 mt-3">
+          <b>–</b>
+        </div>
+        <div class="end-date flex flex-col text-center p-1">
+          <b>
+            {months[date_end.getMonth()].toUpperCase()}
+          </b>
+          <b>
+            {zeroPad(date_end.getDate())}
+          </b>
+        </div>
+        <!-- Badges -->
+        <div class="badges flex flex-col p-1">
+          <GameBadge game={tournament.game} />
+          <ModeBadge mode={tournament.mode} />
+          <TypeBadge type={tournament_type} />
+        </div>
+      </div>
+    </div>
+
+    <!-- Logo Section -->
+    <div class="logo flex flex-col justify-center items-center text-center">
+      {#if tournament.logo != null}
+        <div class="font-bold">
+          <a href="/{$page.params.lang}/tournaments/series/details?id={tournament.series_id}">
+            {#if tournament.series_name != null}
+              {tournament.series_name}
+            {/if}
+          </a>
+        </div>
+        <a href="/{$page.params.lang}/tournaments/series/details?id={tournament.series_id}">
+          <img src={tournament.logo} alt={tournament.name} />
+        </a>
+      {/if}
+    </div>
+
+    <!-- Description & CTA section -->
+    <div class="cta flex flex-col justify-center">
+      <div class="description">
+        {#if tournament.series_id != null}
+          <div class="text-gray-300 p-1">{tournament.series_description}</div>
+        {/if}
+      </div>
+      {#if tournament.registrations_open}
+        <div class="register-button p-1">
+          <Button size="sm" color="yellow" href="/{$page.params.leng}/tournaments/details?id={tournament.id}">
+            <b>Register now!</b>
+          </Button>
+        </div>
+      {:else}
+        <div class="view-tournament-button p-1">
+          <Button size="sm" href="/{$page.params.lang}/tournaments/details?id={tournament.id}"
+            ><b>View Tournament</b></Button
+          >
+        </div>
+      {/if}
+    </div>
   </div>
-  <GameBadge game={tournament.game}/>
-  <ModeBadge mode={tournament.mode}/>
-  <TypeBadge type={tournament_type}/>
-  <div>{months[date_start.getMonth()]} {date_start.getDate()}-{months[date_end.getMonth()]} {date_end.getDate()}</div>
-  {#if tournament.logo != null}
-    <div><img src={tournament.logo} alt={tournament.name} /></div>
-  {/if}
-  {#if tournament.series_id != null}
-    <div>Series {tournament.series_id} - {tournament.series_name}</div>
-    <div>{tournament.series_description}</div>
-  {/if}
 </div>
 
 <style>
   .container {
-    display: grid;
-    background-color: rgba(24, 82, 28, 0.8);
+    background-color: rgba(29, 33, 33, 0.8);
     padding-top: 10px;
     padding-bottom: 10px;
     margin: 10px auto 10px auto;
   }
-  .name {
-    display: inline-block;
+  .container:nth-child(odd) {
+    background-color: rgba(33, 39, 39, 0.8);
   }
   img {
-    max-width: 400px;
-    max-height: 200px;
+    max-width: 76px;
+    max-height: 76px;
+  }
+  b {
+    font-size: 1.2rem;
+    font-weight: 900;
+  }
+  .dates {
+    font-family: monospace;
+  }
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .description {
+    font-size: 0.9rem;
+  }
+  @media (max-width: 768px) {
+    .grid-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .description {
+      display: none;
+    }
+    .cta {
+      text-align: center;
+    }
   }
 </style>
