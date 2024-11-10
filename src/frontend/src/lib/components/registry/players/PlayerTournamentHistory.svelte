a7302a6c758fe6eebe357861c086d1fc192e8e48
billypom on debian <billypompey@icloud.com>
2024-11-09 23:15:25 -0500

    table, filters, and sorting

--- src/frontend/src/lib/components/registry/players/PlayerTournamentHistory.svelte
+++ src/frontend/src/lib/components/registry/players/PlayerTournamentHistory.svelte
@@ -15,7 +15,6 @@
   import { game_order } from '$lib/util/util';
   import GameModeSelect from '$lib/components/common/GameModeSelect.svelte';
   import PlayerName from '$lib/components/tournaments/registration/PlayerName.svelte';
-  import SoloTournamentFields from '$lib/components/tournaments/registration/SoloTournamentFields.svelte';
 
   let user_info: UserInfo;
 
@@ -29,101 +28,196 @@
   let game: string | null = null;
   let from: string | null = null;
   let to: string | null = null;
-  let player_placements: PlayerTournamentPlacement[] = [];
   let solo_placements: PlayerTournamentPlacement[] = [];
-  let squad_placements: PlayerTournamentPlacement[] = [];
   let team_placements: PlayerTournamentPlacement[] = [];
+  let filtered_solo_placements: PlayerTournamentPlacement[] = [];
+  let filtered_team_placements: PlayerTournamentPlacement[] = [];
+  let podium_style: { [key: number]: string } = { 1: 'bg-amber-400/60', 2: 'bg-slate-400/60', 3: 'bg-amber-700/60' };
 
   let avatar_url = logo;
   if (player.user_settings && player.user_settings.avatar) {
     avatar_url = player.user_settings.avatar;
   }
 
-  async function fetchData() {
-    let url = `/api/tournaments/players/placements/${player.id}`;
-    // if (game !== null) {
-    //   url += `&game=${game}`;
-    // }
-    // if (mode !== null) {
-    //   url += `&mode=${mode}`;
-    // }
-    // if (from) {
-    //   url += `&from_date=${new Date(from).getTime() / 1000}`;
-    // }
-    // if (to) {
-    //   url += `&to_date=${new Date(to).getTime() / 1000}`;
-    // }
+  function toDate(unix_timestamp: number) {
+    return new Date(unix_timestamp * 1000).toLocaleDateString();
+  }
 
+  async function applySort() {}
+
+  async function fetchData() {
+    // API
+    let url = `/api/tournaments/players/placements/${player.id}`;
     const res = await fetch(url);
     if (res.status !== 200) {
       return;
     }
     let body = await res.json();
-    solo_placements = body.tournament_solo_placements;
-    squad_placements = body.tournament_squad_placements;
     team_placements = body.tournament_team_placements;
+    solo_placements = body.tournament_solo_and_squad_placements;
+
+    // Filtering
+    filtered_team_placements = [...team_placements];
+    filtered_solo_placements = [...solo_placements];
+
+    if (game) {
+      filtered_team_placements = filtered_team_placements.filter((item) => item.game === game);
+      filtered_solo_placements = filtered_solo_placements.filter((item) => item.game === game);
+    }
+    if (mode) {
+      filtered_team_placements = filtered_team_placements.filter((item) => item.mode === mode);
+      filtered_solo_placements = filtered_solo_placements.filter((item) => item.mode === mode);
+    }
+    if (from) {
+      filtered_solo_placements = filtered_solo_placements.filter((item) => {
+        return item.date_start >= Date.parse(from) / 1000;
+      });
+    }
+    if (to) {
+      filtered_team_placements = filtered_team_placements.filter((item) => {
+        return item.date_end <= Date.parse(to) / 1000;
+      });
+    }
+
+    // Sorting
+    filtered_solo_placements = filtered_solo_placements.sort((a, b) => {
+      return b.date_start - a.date_start;
+    });
+
+    filtered_team_placements = filtered_team_placements.sort((a, b) => {
+      return b.date_start - a.date_start;
+    });
   }
 
   onMount(fetchData);
-  // console.log(player);
 </script>
 
 <Section header={$LL.PLAYER_TOURNAMENT_HISTORY.TOURNAMENT_HISTORY()}>
   <div>
-    <div>
-      <form on:submit|preventDefault={fetchData}>
-        <div class="flex">
-          <GameModeSelect bind:game bind:mode flex all_option hide_labels inline is_team />
-          <Button type="submit">Filter</Button>
+    <form on:submit|preventDefault={fetchData}>
+      <div class="filters flex">
+        <GameModeSelect bind:game bind:mode flex all_option hide_labels inline is_team />
+        <div class="filters flex">
           <div class="option">
-            <label for="from">From</label>
             <input name="from" type="datetime-local" bind:value={from} />
           </div>
+          <div class="text-xl font-bold mx-1">
+            {'-'}
+          </div>
           <div class="option">
-            <label for="to">To</label>
             <input name="to" type="datetime-local" bind:value={to} />
           </div>
-          <div class="option">
-            <Button type="submit">Filter</Button>
-          </div>
         </div>
-      </form>
-      <div>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th class="mobile-hide">Date</th>
-              <th class="mobile-hide">Partners</th>
-              <th>Placement</th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each solo_placements as placement, i}
-              <tr class="row-{i % 2}">
-                <td> {placement.name} </td>
-                <td> {placement.date_end}</td>
-                {#if placement.partners != null}
-                  <td>
-                    {#each JSON.parse(placement.partners) as partner}
-                      <!-- heyyy -->
-                      <!-- {console.log('partner:' + partner.player_id + partner.name)} -->
-                      <div class="flex flex-row">
-                        <div class="hover:text-emerald-400">
-                          <PlayerName player_id={partner.player_id} name={partner.name} />
-                        </div>
-                      </div>
-                    {/each}
-                  </td>
-                {:else}
-                  <td></td>
-                {/if}
-                <td> {placement.placement}</td>
-              </tr>
-            {/each}
-          </tbody>
-        </Table>
       </div>
+      <div class="option my-2">
+        <Button type="submit">Filter</Button>
+      </div>
+    </form>
+    <!-- Solo Tournaments -->
+    <h2 class="text-2xl font-bold">Solo Tournaments</h2>
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th class="mobile-hide">Date</th>
+            <th class="mobile-hide">Partners</th>
+            <th>Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each filtered_solo_placements as placement, i}
+            <tr class={placement.placement ? podium_style[placement.placement] : `row-{i % 2}`}>
+              <td>
+                <a
+                  class="hover:text-emerald-400"
+                  href="/{$page.params.lang}/tournaments/details?id={placement.tournament_id}"
+                >
+                  {placement.tournament_name}
+                </a>
+              </td>
+              <td class="mobile-hide">
+                {toDate(placement.date_start)}
+                {placement.date_end == placement.date_start ? '' : ' - ' + toDate(placement.date_end)}
+              </td>
+              {#if placement.partners != null}
+                <td class="mobile-hide">
+                  {#each placement.partners as partner}
+                    <div class="flex flex-row">
+                      <div class="hover:text-emerald-400">
+                        <PlayerName player_id={partner.player_id} name={partner.player_name} />
+                      </div>
+                    </div>
+                  {/each}
+                </td>
+              {:else}
+                <td></td>
+              {/if}
+              <td>
+                {placement.placement ? placement.placement : '-'}
+                {placement.placement_description ? ' - ' + placement.placement_description : ''}
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </Table>
     </div>
-  </div></Section
->
+
+    <!-- Team Tournaments -->
+    <h2 class="text-2xl font-bold">Team Tournaments</h2>
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th class="mobile-hide">Date</th>
+            <th class="mobile-hide">Team</th>
+            <th>Placement</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each filtered_team_placements as placement, i}
+            <tr class={placement.placement ? podium_style[placement.placement] : `row-{i % 2}`}>
+              <td>
+                <a
+                  class="hover:text-emerald-400"
+                  href="/{$page.params.lang}/tournaments/details?id={placement.tournament_id}"
+                >
+                  {placement.tournament_name}
+                </a>
+              </td>
+              <td class="mobile-hide">
+                {toDate(placement.date_start)}
+                {placement.date_end == placement.date_start ? '' : ' - ' + toDate(placement.date_end)}
+              </td>
+              {#if placement.squad_id != null && placement.squad_name != null}
+                <td class="mobile-hide">
+                  <a
+                    class="hover:text-emerald-400"
+                    href="/{$page.params.lang}/registry/teams/profile?id={placement.team_id}"
+                  >
+                    {placement.squad_name}
+                  </a>
+                </td>
+              {:else}
+                <td></td>
+              {/if}
+              <td>
+                {placement.placement ? placement.placement : '-'}
+                {placement.placement_description ? ' - ' + placement.placement_description : ''}
+              </td>
+            </tr>
+          {/each}
+        </tbody>
+      </Table>
+    </div>
+  </div>
+</Section>
+
+<style>
+  @media screen and (max-width: 768px) {
+    .filters {
+      flex-direction: column;
+    }
+  }
+</style>
