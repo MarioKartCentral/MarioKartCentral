8f4a05667250b9fd70536dfd787bf25014bdfde2
billypom on debian <billypompey@icloud.com>
2024-11-14 18:31:54 -0500

    player transfer/registration history component completed

--- src/frontend/src/lib/types/player-transfer.ts
+++ src/frontend/src/lib/types/player-transfer.ts
@@ -1,13 +1,9 @@
 export type PlayerTransferItem = {
   team_id: number;
   team_name: string;
-  roster_id: number;
-  roster_name: string | null;
-  roster_lead_id: number | null;
   join_date: number;
   leave_date: number | null;
-  is_accepted: boolean;
-  is_bagger_clause: boolean;
+  roster_name: string | null;
 }
 
 export type PlayerTransferHistory = {
