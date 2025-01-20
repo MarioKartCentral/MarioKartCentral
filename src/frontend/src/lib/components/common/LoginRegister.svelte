d4119ebe5b0078a2f98ccc68ea96fdcb7e0abd57
cyndaquilx <ultramk8@gmail.com>
2025-01-20 00:31:43 +0000

    attempt adding thumbmarkjs

--- src/frontend/src/lib/components/common/LoginRegister.svelte
+++ src/frontend/src/lib/components/common/LoginRegister.svelte
@@ -2,6 +2,10 @@
     import LL from '$i18n/i18n-svelte';
     import { page } from '$app/stores';
     import Button from './buttons/Button.svelte';
+    import { getFingerprint } from "@thumbmarkjs/thumbmarkjs";
+
+    const fingerprint = getFingerprint();
+    console.log(fingerprint);
 
     async function loginOrSignup(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
         const data = new FormData(event.currentTarget);
