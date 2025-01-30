f396e7ffa51abb3720676424ae3e4f867dc91db3
Jimmy <56059993+jimmylam1@users.noreply.github.com>
2025-01-29 20:06:55 -0800

    remove login and user info from homepage

--- src/frontend/src/routes/[lang]/+page.svelte
+++ src/frontend/src/routes/[lang]/+page.svelte
@@ -1,42 +1,10 @@
 <script lang="ts">
   import LL from '$i18n/i18n-svelte';
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-  import Button from '$lib/components/common/buttons/Button.svelte';
-  export let userId = 'Loading...';
-  export let playerId = '';
-  export let isLoggedIn = false;
-  import LoginRegister from '$lib/components/common/LoginRegister.svelte';
   import RecentTransactions from '$lib/components/homepage/RecentTransactions.svelte';
   import LatestTournaments from '$lib/components/homepage/LatestTournaments.svelte';
   import LatestResults from '$lib/components/homepage/LatestResults.svelte';
   import NewestTeams from '$lib/components/homepage/NewestTeams.svelte';
   import NewestPlayers from '$lib/components/homepage/NewestPlayers.svelte';
-
-  onMount(async () => {
-    const res = await fetch(`/api/user/me`);
-    if (res.status === 200) {
-      const body = await res.json();
-      userId = body['id'];
-      playerId = body['player_id'] || 'User has not completed player registration';
-      isLoggedIn = true;
-    } else {
-      userId = 'Not logged in';
-      playerId = 'N/A';
-      isLoggedIn = false;
-    }
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async function logout(event: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) {
-    const response = await fetch('/api/user/logout', { method: 'POST' });
-
-    if (response.status < 300) {
-      goto('/');
-    } else {
-      alert($LL.LOGIN.LOGOUT_FAILED());
-    }
-  }
 </script>
 
 <svelte:head>
@@ -55,20 +23,6 @@
   <NewestTeams style='grid-area: d;' />
   <RecentTransactions style='grid-area: e;' />
 </div>
-
-
-<!-- TODO: remove below -->
-<p>{$LL.COMMON.LANGUAGE()}: {$LL.LANGUAGE()}</p>
-
-<p>User ID: {userId}</p>
-<p>Player ID: {playerId}</p>
-{#if isLoggedIn}
-  <form method="post" on:submit|preventDefault={logout}>
-    <Button type="submit">{$LL.LOGIN.LOGOUT()}</Button>
-  </form>
-{:else}
-  <LoginRegister/>
-{/if}
 
 <style>
   .welcome {
@@ -107,5 +61,4 @@
       padding: 0 20px;
     }
   }
-
 </style>
