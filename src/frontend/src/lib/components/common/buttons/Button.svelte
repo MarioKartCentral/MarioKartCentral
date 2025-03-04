92ffef87d5cc34bbefaadd9950b0a461402cffe2
devotyz <8927764+devotyz@users.noreply.github.com>
2025-03-04 15:57:24 +0000

    fix merge error

--- src/frontend/src/lib/components/common/buttons/Button.svelte
+++ src/frontend/src/lib/components/common/buttons/Button.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { Button } from 'flowbite-svelte';
-  import { Button } from 'flowbite-svelte';
 
   export let href: string | undefined = undefined;
   export let circle = false;
@@ -20,27 +19,15 @@
     | 'alternative'
     | 'none'
     | undefined = undefined;
-  export let href: string | undefined = undefined;
-  export let circle = false;
-  export let type: 'submit' | 'button' | 'reset' | null | undefined = 'button';
-  export let size: 'xs' | 'sm' | 'lg' | 'xl' | 'md' = 'sm';
-  export let disabled = false;
-  export let color:
-    | 'red'
-    | 'green'
-    | 'blue'
-    | 'yellow'
-    | 'purple'
-    | 'light'
-    | 'dark'
-    | 'primary'
-    | 'alternative'
-    | 'none'
-    | undefined = undefined;
 </script>
 
-<Button pill={circle} class="{extra_classes} {circle ? '!p-2' : ''}" on:click {size} {href} {type} {disabled} {color}>
-  <slot />
-<Button pill={circle} class={circle ? '!p-2' : ''} on:click {size} {href} {type} {disabled} {color}>
-  <slot />
-</Button>
+<Button
+  pill={circle}
+  class="{extra_classes} {circle ? '!p-2' : ''}"
+  on:click
+  {size}
+  {href}
+  {type}
+  {disabled}
+  {color}
+></Button>
