33d1bb65597814fc8d2026655c920775607a375d
cyndaquilx <ultramk8@gmail.com>
2025-03-05 18:01:35 +0000

    format + add new pages to svelte config

--- src/frontend/src/lib/types/posts.ts
+++ src/frontend/src/lib/types/posts.ts
@@ -1,20 +1,20 @@
-import type { PlayerBasic } from "./player";
+import type { PlayerBasic } from './player';
 
 export type PostBasic = {
-    id: number;
-    title: string;
-    is_public: boolean;
-    is_global: boolean;
-    creation_date: number;
-    created_by: PlayerBasic | null;
-}
+  id: number;
+  title: string;
+  is_public: boolean;
+  is_global: boolean;
+  creation_date: number;
+  created_by: PlayerBasic | null;
+};
 
 export type Post = PostBasic & {
-    content: string;
-}
+  content: string;
+};
 
 export type PostList = {
-    posts: PostBasic[];
-    count: number;
-    page_count: number;
-}
+  posts: PostBasic[];
+  count: number;
+  page_count: number;
+};
