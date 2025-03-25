8a7337e4d204d2d4d3a5e6a9dd850d790b01b9d1
cyndaquilx <ultramk8@gmail.com>
2025-03-25 00:55:57 +0000

    update tab styling

--- src/frontend/src/lib/components/common/tabs/Tabs.svelte
+++ src/frontend/src/lib/components/common/tabs/Tabs.svelte
@@ -2,8 +2,8 @@
     import { Tabs } from "flowbite-svelte";
 </script>
 
-<Tabs tabStyle="full" defaultClass="flex rounded-lg shadow-sm" contentClass='p-4 rounded-lg bg-gray-700 mt-4' 
-    activeClasses='p-4 w-full focus:outline-hidden bg-gray-800 text-white'
-    inactiveClasses='p-4 w-full text-gray-400 focus:outline-hidden hover:text-white bg-gray-700 hover:bg-gray-800'>
+<Tabs tabStyle="full" defaultClass="flex rounded-lg shadow-sm" contentClass='p-4 bg-gray-700' 
+    activeClasses='p-4 w-full focus:outline-hidden bg-gray-700 text-white'
+    inactiveClasses='p-4 w-full text-gray-400 focus:outline-hidden hover:text-white bg-gray-600 hover:bg-gray-700'>
     <slot/>
 </Tabs>
