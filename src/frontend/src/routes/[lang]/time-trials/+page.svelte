9b46dc07ecff8c5c26e2a5e060edf27aa2771b70
cyndaquilx <ultramk8@gmail.com>
2025-04-09 19:20:59 +0000

    update tt page

--- src/frontend/src/routes/[lang]/time-trials/+page.svelte
+++ src/frontend/src/routes/[lang]/time-trials/+page.svelte
@@ -5,19 +5,11 @@
   import { DiscordSolid, LinkSolid } from 'flowbite-svelte-icons';
 </script>
 
-<p>
-  This page is currently a work in progress. For now,
-  players can find leaderboards on several other sites.
-</p>
-
-<!--
-  This page is currently a work in progress, but it will eventually be the hub for everything time trials. For now,
-  players can find leaderboards on several other sites. This is the hub of Mario Kart Time Trials. Here, you'll find
-  the most up-to-date rankings for Mario Kart 8 Deluxe, Mario Kart 8, Mario Kart 7, and Mario Kart Wii, with every
-  category imagineable. Submit your times to see where you stack up!
--->
-
-<main class="container">
+<div class="tt-container">
+  <div class="disclaimer">
+    This page is currently a work in progress. For now,
+    players can find leaderboards on several other sites.
+  </div>
   <GameBadge game= "mk8dx" />
   <GameBadge game= "mk7" />
   <GameBadge game= "mkw" />
@@ -81,44 +73,14 @@
       </div>
     </div>
   </Section>
-</main>
- 
-<!--
-  <div style="display: flex; flex-wrap: wrap; gap: 10px; justify-content: center;">
-    <a href="/{$page.params.lang}/time-trials/mk8dx" style="flex: 1 1 calc(33.333% - 10px); text-decoration: none;">
-      <Button
-        style="width: 100%; padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;"
-      >
-        <img src={MK8DX} alt="Mario Kart 8 Deluxe" style="max-width: 100%;" />
-      </Button>
-    </a>
-    <a href="/{$page.params.lang}/time-trials/mk8" style="flex: 1 1 calc(33.333% - 10px); text-decoration: none;">
-      <Button
-        style="width: 100%; padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;"
-      >
-        <img src={MK8} alt="Mario Kart 8" style="max-width: 100%;" />
-      </Button>
-    </a>
-    <a href="/{$page.params.lang}/time-trials/mk7" style="flex: 1 1 calc(33.333% - 10px); text-decoration: none;">
-      <Button
-        style="width: 100%; padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;"
-      >
-        <img src={MK7} alt="Mario Kart 7" style="max-width: 100%;" />
-      </Button>
-    </a>
-    <a href="/{$page.params.lang}/time-trials/mkw" style="flex: 1 1 calc(33.333% - 10px); text-decoration: none;">
-      <Button
-        style="width: 100%; padding: 10px; border: none; background-color: transparent; display: flex; justify-content: center; align-items: center;"
-      >
-        <img src={MKW} alt="Mario Kart Wii" style="max-width: 100%;" />
-      </Button>
-    </a>
-  </div>
--->
+</div>
 
 <style>
-  main.container {
+  .tt-container {
     max-width: 1200px;
     margin: 20px auto 20px auto;
+  }
+  .disclaimer {
+    margin-bottom: 20px;
   }
 </style>
