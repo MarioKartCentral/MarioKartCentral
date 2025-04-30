ef5cd9c2c33e4f340b4586371058e9aac9a73c92
cyndaquilx <ultramk8@gmail.com>
2025-04-30 04:09:59 +0000

    fix unneeded data being submitted from edit tournament page

--- src/frontend/src/routes/[lang]/tournaments/edit/+page.svelte
+++ src/frontend/src/routes/[lang]/tournaments/edit/+page.svelte
@@ -24,7 +24,51 @@
     if (res.status === 200) {
       const body: Tournament = await res.json();
       tournament = body;
-      data = {...body, logo_file: null, remove_logo: false,};
+      data = {
+        name: tournament.name,
+        series_id: tournament.series_id,
+        date_start: tournament.date_start,
+        date_end: tournament.date_end,
+        logo: tournament.logo,
+        use_series_logo: tournament.use_series_logo,
+        url: tournament.url,
+        organizer: tournament.organizer,
+        location: tournament.location,
+        game: tournament.game,
+        mode: tournament.mode,
+        is_squad: tournament.is_squad,
+        min_squad_size: tournament.min_squad_size,
+        max_squad_size: tournament.max_squad_size,
+        squad_name_required: tournament.squad_name_required,
+        squad_tag_required: tournament.squad_tag_required,
+        teams_allowed: tournament.teams_allowed,
+        teams_only: tournament.teams_only,
+        team_members_only: tournament.team_members_only,
+        min_representatives: tournament.min_representatives,
+        host_status_required: tournament.host_status_required,
+        mii_name_required: tournament.mii_name_required,
+        require_single_fc: tournament.require_single_fc,
+        checkins_enabled: tournament.checkins_enabled,
+        checkins_open: tournament.checkins_open,
+        min_players_checkin: tournament.min_players_checkin,
+        verification_required: tournament.verification_required,
+        description: tournament.description,
+        use_series_description: tournament.use_series_description,
+        ruleset: tournament.ruleset,
+        use_series_ruleset: tournament.use_series_ruleset,
+        registrations_open: tournament.registrations_open,
+        registration_deadline: tournament.registration_deadline,
+        registration_cap: tournament.registration_cap,
+        is_viewable: tournament.is_viewable,
+        is_public: tournament.is_public,
+        is_deleted: tournament.is_deleted,
+        show_on_profiles: tournament.show_on_profiles,
+        series_stats_include: tournament.series_stats_include,
+        verified_fc_required: tournament.verified_fc_required,
+        bagger_clause_enabled: tournament.bagger_clause_enabled,
+        logo_file: null,
+        remove_logo: false,
+      };
     }
   });
 </script>
