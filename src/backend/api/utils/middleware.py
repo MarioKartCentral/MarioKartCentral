d4c3b4bc3704a15a6662ab7c54002381bb0829e1
cyndaquilx <ultramk8@gmail.com>
2025-05-14 23:35:53 +0000

    fix linter errors

--- src/backend/api/utils/middleware.py
+++ src/backend/api/utils/middleware.py
@@ -9,14 +9,16 @@ from ratelimit.types import ASGIApp, Scope, Receive, Send
 from ratelimit import RateLimitMiddleware, Rule
 from ratelimit.backends.simple import MemoryBackend
 import ipaddress
+from typing import Any
+from starlette.requests import Request
 
-async def handle_error(request, exception):
+async def handle_error(request: Request, exception: Exception):
     return ProblemResponse(Problem("Unexpected Error"))
 
-async def handle_problem(request, problem):
+async def handle_problem(request: Request, problem: Problem):
     return ProblemResponse(problem)
 
-exception_handlers = {
+exception_handlers: dict[Any, Any] = {
     Problem: handle_problem,
     500: handle_error
 }
