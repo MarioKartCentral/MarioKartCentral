7ddbeac6d3714475b325812734c1679a3eb2e760
cyndaquilx <ultramk8@gmail.com>
2025-05-19 00:46:16 +0000

    fix styling on hometournamentpageitem

--- src/frontend/src/lib/components/homepage/HomeTournamentPageItem.svelte
+++ src/frontend/src/lib/components/homepage/HomeTournamentPageItem.svelte
@@ -46,7 +46,7 @@
         <!-- badges -->
         <div class="badges flex gap-[5px] flex-wrap mobile-center">
             <GameBadge game={tournament.game} style='font-size: 0.95rem;' />
-            <ModeBadge mode={tournament.mode} style='font-size: 0.95rem;' />
+            <ModeBadge mode={tournament.mode} style='font-size: 0.95rem;width:120px;' />
             <TypeBadge is_squad={tournament.is_squad} teams_allowed={tournament.teams_allowed} style='font-size: 0.95rem;' />
             <OrganizerBadge organizer={tournament.organizer} style='font-size: 0.95rem;'/>
           </div>
