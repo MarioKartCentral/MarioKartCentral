0f2e5719739931a9bfcfcee19c08fade1bfe150c
leer97 <84957936+leer97@users.noreply.github.com>
2025-10-14 13:55:20 +0100

    refactor: migrate to eslint.config.js per eslint v9

---
+++ src/frontend/eslint.config.js
@@ -0,0 +1,61 @@
+import prettier from 'eslint-config-prettier';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      '@typescript-eslint': ts.plugin,
+    },
+    rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig,
+      },
+    },
+    rules: {
+      'svelte/no-navigation-without-resolve': 'off',
+      // these are linting errors caused by $: reactive statements in Svelte 4
+      'svelte/require-event-dispatcher-types': 'warn',
+      'svelte/no-reactive-reassign': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+    },
+  },
+  {
+    ignores: [
+      '**/*.cjs',
+      'package',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/yarn.lock',
+      '**/i18n/i18n-*?(.*).ts',
+    ],
+  },
+);
