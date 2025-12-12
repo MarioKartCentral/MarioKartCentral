cd68161cda34b6cb371c394aaf6b665d279c7cef
leer97 <84957936+leer97@users.noreply.github.com>
2025-12-12 14:36:38 +0000

    style: remove dropdown outer padding

--- src/frontend/src/lib/components/common/Dropdown.svelte
+++ src/frontend/src/lib/components/common/Dropdown.svelte
@@ -6,6 +6,6 @@
   export let scroll = false;
 </script>
 
-<Dropdown class="bg-gray-600 text-white {scroll ? 'max-h-64 overflow-y-auto' : ''}" {placement} bind:open>
+<Dropdown class="bg-gray-600 text-white py-0 {scroll ? 'max-h-64 overflow-y-auto' : ''}" {placement} bind:open>
   <slot />
 </Dropdown>
