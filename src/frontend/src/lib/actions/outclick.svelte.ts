ec26acf02a46ce03ccfc8d7327faed9de5c4cbf5
leer97 <84957936+leer97@users.noreply.github.com>
2025-12-23 12:56:10 +0000

    feat: create outclick action

---
+++ src/frontend/src/lib/actions/outclick.svelte.ts
@@ -0,0 +1,39 @@
+import type { Action } from 'svelte/action';
+
+/**
+ * Observes focus changes and dispatches a custom `outclick` event if the focussed element is outside the target element or its children.
+ *
+ * - Add `use:clickOutside` to the element to observe
+ * - Listen to the event with `on:outclick`
+ * @param node The root element to observe
+ * @param active Determines whether the event listener is active. Bindable to a variable with `use:clickOutside={active}`. Defaults to `true`
+ */
+export const clickOutside: Action<HTMLElement, boolean | undefined, { 'on:outclick': (e: CustomEvent) => void }> = (
+  node: HTMLElement,
+  active: boolean = true,
+) => {
+  function checkOutclick(event: MouseEvent | FocusEvent) {
+    if (!event.target) throw Error('Event target not found');
+    if (node && !node.contains(event.target as Node)) {
+      node.dispatchEvent(new CustomEvent('outclick'));
+    }
+  }
+
+  function addListeners() {
+    document.addEventListener('click', checkOutclick);
+    document.addEventListener('focusin', checkOutclick);
+  }
+  function removeListeners() {
+    document.removeEventListener('click', checkOutclick);
+    document.removeEventListener('focusin', checkOutclick);
+  }
+  if (active) {
+    addListeners();
+  }
+  return {
+    destroy: () => {
+      removeListeners();
+    },
+    update: (active) => (active ? addListeners() : removeListeners()),
+  };
+};
