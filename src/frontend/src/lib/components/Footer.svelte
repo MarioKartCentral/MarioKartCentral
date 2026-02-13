3f8a111a4941a13c2613e70a306fc6809378fa8d
leer97 <84957936+leer97@users.noreply.github.com>
2026-02-13 15:54:42 +0000

    style: update footer

--- src/frontend/src/lib/components/Footer.svelte
+++ src/frontend/src/lib/components/Footer.svelte
@@ -4,63 +4,48 @@
   import Twitch from '$lib/components/icons/Twitch.svelte';
 </script>
 
-<div class="footer">
-  <div class="footer-social">
-    <ul>
+<div class="footer p-2">
+  <div class="flex flex-col sm:flex-row gap-2">
+    <ul class="flex flex-row gap-2 items-center">
       <li>
-        <a href="https://www.youtube.com/c/MarioKartCentral" rel="external" title="YouTube">
-          <YoutubeSolid />
+        <a href="https://www.youtube.com/c/MarioKartCentral" rel="external" title="YouTube" aria-label="YouTube">
+          <YoutubeSolid size="lg" aria-hidden="true" />
         </a>
       </li>
       <li>
-        <a href="https://twitter.com/mk_central" rel="external" title="Twitter">
-          <XSolid />
+        <a href="https://twitter.com/mk_central" rel="external" title="Twitter" aria-label="X (formerly Twitter)">
+          <XSolid size="lg" aria-hidden="true" />
         </a>
       </li>
       <li>
-        <a href="https://www.twitch.tv/mariokartcentral" rel="external" title="Twitch">
-          <Twitch />
+        <a href="https://www.twitch.tv/mariokartcentral" rel="external" title="Twitch" aria-label="Twitch">
+          <Twitch size="md" />
         </a>
       </li>
       <li>
-        <a href="https://github.com/MarioKartCentral/MarioKartCentral" rel="external" title="Github">
-          <GithubSolid />
+        <a
+          href="https://github.com/MarioKartCentral/MarioKartCentral"
+          rel="external"
+          title="Github"
+          aria-label="GitHub"
+        >
+          <GithubSolid size="lg" aria-hidden="true" />
         </a>
       </li>
     </ul>
+    <ul class="flex flex-col gap-2 sm:text-end sm:flex-row sm:ml-auto sm:items-center">
+      <li><a href="/{$page.params.lang}/about">About Us</a></li>
+      <li><a href="/{$page.params.lang}/user/terms">Terms and Rules</a></li>
+      <li><a href="/{$page.params.lang}/user/privacy-policy">Privacy Policy</a></li>
+    </ul>
   </div>
-  <div class="footer-about">
-    <a href="/{$page.params.lang}/about">About Us</a>
-  </div>
-  <div class="footer-terms">
-    <a href="/{$page.params.lang}/user/terms">Terms and Rules</a>
-  </div>
-  <div class="footer-privacy">
-    <a href="/{$page.params.lang}/user/privacy-policy">Privacy Policy</a>
-  </div>
-  <div class="footer-copyright">&copy; 2018-2025 mkcentral.com Mario Kart&trade; and &copy; Nintendo.</div>
+  <div class="text-end text-gray-400">&copy; 2018-2026 mkcentral.com Mario Kart&trade; and &copy; Nintendo.</div>
 </div>
 
 <style>
   .footer {
-    display: flex;
     background-color: #313030;
     font-size: 14px;
-    gap: 15px;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    padding: 5px;
-  }
-  .footer-social {
-    flex: 1;
-    min-width: max-content;
-  }
-  .footer-social ul {
-    display: flex;
-    list-style: none;
-    gap: 10px;
-    align-items: center;
   }
 
   a {
--- src/frontend/src/lib/components/icons/Twitch.svelte
+++ src/frontend/src/lib/components/icons/Twitch.svelte
@@ -1,10 +1,25 @@
+<script lang="ts">
+  type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  export let size: Size = 'md';
+  const sizes: Record<Size, number> = {
+    xs: 12,
+    sm: 16,
+    md: 20,
+    lg: 24,
+    xl: 28,
+  };
+
+  $: value = sizes[size];
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
-  width="16"
-  height="16"
+  width={value}
+  height={value}
   fill="currentColor"
   class="bi bi-twitch"
   viewBox="0 0 16 16"
+  aria-hidden="true"
 >
   <path
     d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z"
