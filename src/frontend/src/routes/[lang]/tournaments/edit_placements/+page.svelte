f4409645879b81f77e904e34aaecb29781074ab5
leer97 <84957936+leer97@users.noreply.github.com>
2026-04-18 14:33:01 +0000

    fix: fix placements page series admin permission

--- src/frontend/src/routes/[lang]/tournaments/edit_placements/+page.svelte
+++ src/frontend/src/routes/[lang]/tournaments/edit_placements/+page.svelte
@@ -4,6 +4,7 @@
   import { page } from '$app/stores';
   import PlacementsDragDropZone from '$lib/components/tournaments/placements/PlacementsDragDropZone.svelte';
   import type { TournamentPlacementList } from '$lib/types/tournament-placement';
+  import type { Tournament } from '$lib/types/tournament';
   import Button from '$lib/components/common/buttons/Button.svelte';
   import LL from '$i18n/i18n-svelte';
   import { check_tournament_permission, tournament_permissions } from '$lib/util/permissions';
@@ -11,16 +12,25 @@
 
   let tournamentId: number;
   let placements: TournamentPlacementList;
+  let tournament: Tournament;
   let isLoaded = false;
 
   onMount(async () => {
     let paramId = $page.url.searchParams.get('id');
     tournamentId = parseInt(paramId || '');
     if (tournamentId) {
-      const response = await fetch(`/api/tournaments/${tournamentId}/placements`);
-      if (response.ok) {
-        placements = await response.json();
-      }
+      await Promise.all([
+        fetch(`/api/tournaments/${tournamentId}`),
+        fetch(`/api/tournaments/${tournamentId}/placements`),
+      ]).then(async ([tournamentResponse, placementResponse]) => {
+        if (tournamentResponse.ok) {
+          tournament = await tournamentResponse.json();
+        }
+
+        if (placementResponse.ok) {
+          placements = await placementResponse.json();
+        }
+      });
     }
     isLoaded = true;
   });
@@ -41,41 +51,43 @@
   }
 </script>
 
-{#if isLoaded}
-  {#if placements && check_tournament_permission($user, tournament_permissions.manage_placements, placements.tournament_id)}
-    <Section header={$LL.TOURNAMENTS.BACK_TO_TOURNAMENT()}>
-      <div slot="header_content">
-        <Button href="/{$page.params.lang}/tournaments/details?id={placements.tournament_id}"
-          >{$LL.COMMON.BACK()}</Button
-        >
-      </div>
-    </Section>
-    <Section header={$LL.TOURNAMENTS.PLACEMENTS.EDIT_PLACEMENTS()}>
-      <div slot="header_content">
-        <Button href="/{$page.params.lang}/tournaments/edit_placements/raw?id={placements.tournament_id}">
-          {$LL.TOURNAMENTS.PLACEMENTS.SWITCH_TO_RAW_INPUT()}
-        </Button>
-        <Button href="/{$page.params.lang}/tournaments/edit_placements/raw_player_id?id={placements.tournament_id}">
-          {$LL.TOURNAMENTS.PLACEMENTS.RAW_INPUT_PLAYER_ID()}
-        </Button>
-        <Button on:click={reset_placements}>Reset</Button>
-      </div>
-      {#key placements.placements}
-        <PlacementsDragDropZone tournament_id={placements.tournament_id} placements={placements.placements} />
-      {/key}
-    </Section>
-    <Section header={$LL.TOURNAMENTS.PLACEMENTS.UNPLACED({ is_squad: true })}>
-      {#key placements.unplaced}
-        <PlacementsDragDropZone
-          tournament_id={placements.tournament_id}
-          placements={placements.unplaced}
-          is_placements={false}
-        />
-      {/key}
-    </Section>
-  {:else if placements}
-    {$LL.COMMON.NO_PERMISSION()}
-  {:else}
-    {$LL.TOURNAMENTS.NOT_FOUND()}
+{#key $user}
+  {#if isLoaded}
+    {#if placements && check_tournament_permission($user, tournament_permissions.manage_placements, placements.tournament_id, tournament.series_id)}
+      <Section header={$LL.TOURNAMENTS.BACK_TO_TOURNAMENT()}>
+        <div slot="header_content">
+          <Button href="/{$page.params.lang}/tournaments/details?id={placements.tournament_id}"
+            >{$LL.COMMON.BACK()}</Button
+          >
+        </div>
+      </Section>
+      <Section header={$LL.TOURNAMENTS.PLACEMENTS.EDIT_PLACEMENTS()}>
+        <div slot="header_content">
+          <Button href="/{$page.params.lang}/tournaments/edit_placements/raw?id={placements.tournament_id}">
+            {$LL.TOURNAMENTS.PLACEMENTS.SWITCH_TO_RAW_INPUT()}
+          </Button>
+          <Button href="/{$page.params.lang}/tournaments/edit_placements/raw_player_id?id={placements.tournament_id}">
+            {$LL.TOURNAMENTS.PLACEMENTS.RAW_INPUT_PLAYER_ID()}
+          </Button>
+          <Button on:click={reset_placements}>Reset</Button>
+        </div>
+        {#key placements.placements}
+          <PlacementsDragDropZone tournament_id={placements.tournament_id} placements={placements.placements} />
+        {/key}
+      </Section>
+      <Section header={$LL.TOURNAMENTS.PLACEMENTS.UNPLACED({ is_squad: true })}>
+        {#key placements.unplaced}
+          <PlacementsDragDropZone
+            tournament_id={placements.tournament_id}
+            placements={placements.unplaced}
+            is_placements={false}
+          />
+        {/key}
+      </Section>
+    {:else if placements}
+      {$LL.COMMON.NO_PERMISSION()}
+    {:else}
+      {$LL.TOURNAMENTS.NOT_FOUND()}
+    {/if}
   {/if}
-{/if}
+{/key}
